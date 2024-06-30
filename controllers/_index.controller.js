1f92117cc68b25226eeaca3c71e0dec43afabe48
msi-end <aditya01377@gmail.com>
2024-06-30 18:47:54 +0530

    new updates

--- controllers/_index.controller.js
+++ controllers/_index.controller.js
@@ -71,17 +71,11 @@ exports.clients_vendors = (req, res) => {
 
 exports.vendor_supplies = (req, res) => {
     if (req.session.isLoggedIn == true && req.session.role == 'admin') {
-        const query = `SELECT vendor_supplies.id,vendor_supplies.vendor_id, vendor_supplies.item_name, vendor_supplies.details, vendor_supplies.sgst, vendor_supplies.cgst,vendor_supplies.total_amount, vendor_supplies.modeofpay, vendor_supplies.date, vendors.name,vendors.contact,vendors.location, COALESCE(sum(vendor_payments.amount),0) as paid FROM vendor_supplies JOIN vendors on vendor_supplies.vendor_id = vendors.id LEFT JOIN vendor_payments ON vendor_supplies.id = vendor_payments.vendor_supply_id GROUP BY vendor_supplies.vendor_id;`
+        const query = `SELECT vendor_supplies.id,vendor_supplies.vendor_id, vendor_supplies.item_name, vendor_supplies.details, vendor_supplies.sgst, vendor_supplies.cgst,vendor_supplies.total_amount, vendor_supplies.modeofpay, vendor_supplies.date, vendors.name,vendors.contact,vendors.location, COALESCE(sum(vendor_payments.amount),0) as paid FROM vendor_supplies JOIN vendors on vendor_supplies.vendor_id = vendors.id LEFT JOIN vendor_payments ON vendor_supplies.id = vendor_payments.vendor_supply_id GROUP BY vendor_payments.vendor_supply_id
+        ;SELECT id, name FROM vendors;`
         db.query(query, (err, result, field) => {
             if (!err) {
-                const vendorQuery = `SELECT id, name FROM vendors;`
-                db.query(vendorQuery, (errVendor, vendorResult, field) => {
-                    if (!errVendor) {
-                        console.log(result);
-                        return res.status(200).render('../views/admin/vendor_supplies.ejs', { data: result, vendorData: vendorResult })
-                    }
-
-                })
+                res.status(200).render('../views/admin/vendor_supplies.ejs', { data: result})
             } else {
                 return res.status(500).send({ msg: "Internal error occurs!" });
             }
@@ -91,7 +85,7 @@ exports.vendor_supplies = (req, res) => {
 
 exports.supply_payments = (req, res) => {
     if (req.session.isLoggedIn == true && req.session.role == 'admin') {
-        const query = `SELECT COALESCE(SUM(vendor_payments.amount), 0) AS total_payments, vendor_supplies.id, vendor_supplies.item_name, vendor_supplies.sgst, vendor_supplies.cgst,vendor_supplies.total_amount, vendor_supplies.date, vendors.name, vendors.contact, vendors.location FROM vendor_supplies JOIN vendors ON vendor_supplies.vendor_id = vendors.id LEFT JOIN vendor_payments ON vendor_supplies.id = vendor_payments.vendor_supply_id GROUP BY vendor_supplies.vendor_id;`
+        const query = `SELECT COALESCE(SUM(vendor_payments.amount), 0) AS total_payments, vendor_supplies.id, vendor_supplies.item_name, vendor_supplies.sgst, vendor_supplies.cgst,vendor_supplies.total_amount, vendor_supplies.date, vendors.name, vendors.contact, vendors.location FROM vendor_supplies JOIN vendors ON vendor_supplies.vendor_id = vendors.id LEFT JOIN vendor_payments ON vendor_supplies.id = vendor_payments.vendor_supply_id GROUP BY vendor_payments.vendor_supply_id;`
         db.query(query, (err, result, field) => {
             res.status(200).render('../views/admin/vendor_payments.ejs', { data: result })
         })
